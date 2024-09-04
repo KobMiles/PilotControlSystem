edb7ad69ea88e0b6ed302e0e981172760588e6a0
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2024-09-04 12:52:14 +0300

    Add HeightIndicator class to PilotControlSystem
    Introduce HeightIndicator class in _20240829_PilotControlSystem namespace.
    - Add necessary using directives.
    - Define HeightIndicator class inheriting from Indicator.
    - Add private field _controlSystem of type ControlSystem.
    - Implement constructor to initialize _controlSystem.
    - Override SendValue method to return height from ControlSystem.
    - Override ToString method to return height as a string.

---
+++ 20240829_PilotControlSystem/Classes/HeightIndicator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20240829_PilotControlSystem
+{
+    class HeightIndicator : Indicator
+    {
+        ControlSystem _controlSystem;
+        public HeightIndicator(ControlSystem controlSystem)
+        {
+            _controlSystem = controlSystem;
+        }
+        public override double SendValue()
+        {
+            return _controlSystem.GetHeight();
+        }
+
+        public override string ToString()
+        {
+            return _controlSystem.GetHeight().ToString();
+        }
+    }
+}
