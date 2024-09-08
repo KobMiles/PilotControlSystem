62c9c757771e8d77fd8c382e85f9606d3a6e2d66
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2024-09-08 19:06:17 +0300

    Refactor FuelSystem class and fix namespace
    - Modified namespace to include nested `Classes` namespace.
    - Added constants: `DelayTimeMilliseconds` and `FuelConsumptionRate`.
    - Replaced `_fuel` field with public property `Fuel` (private setter).
    - Changed `_engine` field to `readonly`.
    - Simplified constructor to initialize `Fuel` to `500.0` and start `FuelConsumptionAsync`.
    - Removed overloaded constructor with `fuel` parameter.
    - Removed `GetFuel` method.
    - Renamed `FuelComsumptionAsync` to `FuelConsumptionAsync`.
    - Updated `FuelConsumptionAsync` to use new constants and `Fuel` property.

--- 20240829_PilotControlSystem/Classes/FuelSystem.cs
+++ 20240829_PilotControlSystem/Classes/FuelSystem.cs
@@ -1,46 +1,39 @@
-﻿namespace _20240829_PilotControlSystem
+﻿namespace _20240829_PilotControlSystem.Classes
 {
     internal class FuelSystem
     {
         #region ---=== Fields ===---
+        private const int DelayTimeMilliseconds = 400;
+        private const double FuelConsumptionRate = 0.00001;
 
-        private double _fuel;
-        private Engine _engine;
+        public double Fuel { get; private set; }
+        private readonly Engine _engine;
 
         #endregion
+
 
         #region ---=== Constructions ===---
 
         public FuelSystem(Engine engine)
         {
             _engine = engine;
-            _fuel = 500.0;
-            _ = FuelComsumptionAsync();
-        }
-
-        public FuelSystem(double fuel, Engine engine)
-        {
-            _fuel = fuel;
-            _engine = engine;
-            _ = FuelComsumptionAsync();
+            Fuel = 500.0;
+            _ = FuelConsumptionAsync();
         }
 
         #endregion
 
         #region ---=== Methods ===---
-        public double GetFuel()
+
+        public async Task FuelConsumptionAsync()
         {
-            return _fuel;
-        }
-        public async Task FuelComsumptionAsync()
-        {
-            while (_fuel != 0)
+            while (Fuel > 0)
             {
-                await Task.Delay(400);
-                _fuel -= _engine.GetRPM() * 0.00001;
+                await Task.Delay(DelayTimeMilliseconds);
+                Fuel -= _engine.GetRpm() * FuelConsumptionRate;
             }
         }
-        #endregion
 
+        #endregion
     }
 }
