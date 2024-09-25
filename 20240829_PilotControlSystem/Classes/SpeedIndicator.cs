b5669e6ef21fd9431539373709f05d3f69f11359
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2024-09-25 19:09:28 +0300

    Refactor SpeedIndicator class and update GetValue method
    - Changed constructor to parameterized using C# 9.0 syntax.
    - Replaced private fields with constants: MaxRpm, MinRpm, MaxSpeed.
    - Simplified GetValue method to use new constants and return speed directly.

--- 20240829_PilotControlSystem/Classes/SpeedIndicator.cs
+++ 20240829_PilotControlSystem/Classes/SpeedIndicator.cs
@@ -1,23 +1,12 @@
 ﻿namespace _20240829_PilotControlSystem.Classes
 {
-    internal class SpeedIndicator : Indicator
+    internal class SpeedIndicator(Engine engine) : Indicator
     {
         #region ---=== Fields ===---
 
-        private Engine _engine;
-
-        private const int MAX_RPM = 6101;
-        private const int MIN_RPM = 3701;
-        private const int MAX_SPEED = 900;
-
-        #endregion
-
-        #region ---=== Constructions ===---
-
-        public SpeedIndicator(Engine engine)
-        {
-            _engine = engine;
-        }
+        private const int MaxRpm = 6101;
+        private const int MinRpm = 3701;
+        private const int MaxSpeed = 900;
 
         #endregion
 
@@ -25,9 +14,8 @@
 
         public override double GetValue()
         {
-            var speed = Math.Round((_engine.GetRpm() - MIN_RPM) / (MAX_RPM - MIN_RPM) * MAX_SPEED);
-            return _engine.GetRpm() < MIN_RPM ? 0.0 : _engine.GetRpm();
-
+            var speed = (engine.GetRpm() - MinRpm) / (MaxRpm - MinRpm) * MaxSpeed;
+            return speed;
         }
 
         #endregion
